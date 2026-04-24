e37d811c30f9dd60fffd84d8f75dcf05fe0399d4
shouydev <shouyestefanero@gmail.com>
2026-04-23 21:09:22 -0500

    chore: initial commit.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from './shared/presentation/components/header/header.vue'
+import Hero from './value-proposition/presentation/components/hero/hero.vue'
+</script>
+
+<template>
+  <Header />
+  <main>
+    <Hero />
+  </main>
+</template>
+
+<style scoped></style>
