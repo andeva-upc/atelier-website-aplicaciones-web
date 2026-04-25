ed19969b3dbf1aba599e41992eea2e3472ca4304
danieltyuyu <di0014617@gmail.com>
2026-04-25 11:22:16 -0500

    feat(benefits): add card component with animation and styling.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Header from './shared/presentation/components/header/header.vue'
 import Hero from './value-proposition/presentation/components/hero/hero.vue'
+import Services from './services/presentation/components/services/services.vue'
 </script>
 
 <template>
   <Header />
   <main>
     <Hero />
+    <Services />
   </main>
 </template>
 
